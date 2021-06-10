bfc490d29db0e02689c6ef8b2682e34643f5977a
tifana rosa <tifanarosa@gmail.com>
2021-06-10 02:39:50 +0200

    first push for this app

---
+++ src/components/layout/Nav.js
@@ -0,0 +1,25 @@
+import React, { Link } from "react";
+
+const Nav = () => {
+  return (
+    <header>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Nav;
